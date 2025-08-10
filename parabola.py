eb6d2985d6e8654adc085355b51523abd0f6e484
Piotr <pklonowicz2@gmail.com>
2025-08-10 12:53:32 +0200

    Initial commit - parabolic plot script

---
+++ parabola.py
@@ -0,0 +1,13 @@
+a = float(input("Podaj a: ")) 
+b = float(input("Podaj b: ")) 
+c = float(input("Podaj c: ")) 
+import matplotlib.pyplot as plt 
+import numpy as np 
+x = np.linspace(-10, 10, 400) 
+y = a*x**2 + b*x + c 
+plt.plot(x, y) 
+plt.xlabel("x") 
+plt.ylabel("y") 
+plt.title("Wykres paraboli") 
+plt.grid(True) 
+plt.show() 
